9f6387cf4bcd7c530ce2e7fe9bd391cb442d67fb
longwatermelon <qianhansenb@gmail.com>
2021-05-01 19:39:18 -0700

    remove old garbage

--- chess-singleplayer/include/chess.h
+++ chess-singleplayer/include/chess.h
@@ -5,37 +5,3 @@ namespace chess
 {
 	void mainloop();
 }
-
-
-#if 0
-#pragma once
-#include <common/include/graphics.h>
-#include <common/include/piece.h>
-#include <memory>
-#include <string>
-#include <vector>
-
-
-namespace chess
-{
-	inline std::unique_ptr<Graphics> m_gfx;
-	inline std::vector<Piece> m_pieces;
-
-	inline Piece* m_selected_piece;
-	inline SDL_Point m_selected_piece_orig;
-
-	inline std::vector<SDL_Point> m_valid_moves;
-
-	inline Color m_turn;
-
-	void init();
-
-	void mainloop();
-
-	void draw_board();
-
-	void handle_mouse(int px, int py, bool mouse_down);
-
-	bool valid_move();
-}
-#endif
